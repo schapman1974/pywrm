5f55b39ddd143b076162d5ff1afe48b69accc584
Stephen Chapman <schapman1974@gmail.com>
2020-04-06 11:27:45 -0400

    add gitpod support

--- pywrm/app/main.py
+++ pywrm/app/main.py
@@ -5,13 +5,13 @@ import os
 import json
 import logging
 
-from starlette.requests import Request
-from starlette.responses import (FileResponse,
-                                 HTMLResponse,
-                                 PlainTextResponse,
-                                 Response)
+from fastapi.requests import Request
+from fastapi.responses import (FileResponse,
+                               HTMLResponse,
+                               PlainTextResponse,
+                               Response)
 
-from starlette.staticfiles import StaticFiles
+from fastapi.staticfiles import StaticFiles
 from fastapi import Depends, FastAPI, Header, HTTPException
 
 logging = logging.getLogger('main_logger')
@@ -22,15 +22,12 @@ app = FastAPI()
 @app.get("/", response_class=HTMLResponse)
 def modulelist(template: str, unique_id: str):
     """ Get a module list from the modules folder and display them."""
-    files = os.listdir("modules")
-    modules = ""
-    for afile in files:
-        modules += f"{afile}<br>"
+    modules = '<br>'.join(os.listdir("modules"))
     return f"<html>{modules}</html>"
     
 @app.get("/{moduleName}", response_class=HTMLResponse)
 def entrypoint(template: str = "main"):
     """ Main template entrypoint"""
-    template_file = os.path.join("static" , "{template}.html")
+    template_file = os.path.join("static" , f"{template}.html")
     main_template = open(template_file, "r").read()
     return main_template
